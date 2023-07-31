981521669bc1a92cb8ee3c8b7378a06f2b402dce
Etxben <sinodejavi@gmail.com>
2023-07-31 09:48:33 +0200

    [19] - Feature envy

---
+++ src/FeatureEnvy/Error/Employee.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Deg540\PHPTestingBoilerplate\FeatureEnvy\Error;
+
+class Employee
+{
+    private string $name;
+    private int $workedHours;
+    private float $pricePerHour;
+    private string $kindOfBranchOffice;
+
+    /**
+     * @param string $name
+     * @param int $workedHours
+     * @param float $pricePerHour
+     */
+    public function __construct(string $name, int $workedHours, float $pricePerHour)
+    {
+        $this->name = $name;
+        $this->workedHours = $workedHours;
+        $this->pricePerHour = $pricePerHour;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getWorkedHours(): int
+    {
+        return $this->workedHours;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPricePerHour(): float
+    {
+        return $this->pricePerHour;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKindOfBranchOffice(): string
+    {
+        return $this->kindOfBranchOffice;
+    }
+
+    /**
+     * @param string $kindOfBranchOffice
+     */
+    public function setKindOfBranchOffice(string $kindOfBranchOffice): void
+    {
+        $this->kindOfBranchOffice = $kindOfBranchOffice;
+    }
+}
