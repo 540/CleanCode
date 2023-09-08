d296e1cc80379bd94e7fcf0af951549dae8cf3d1
Asier Alba Osambela <asikowndo@gmail.com>
2023-09-08 08:18:45 +0000

    [19] - Eliminado los comentarios de param de las funciones

--- src/FeatureEnvy/Trial/Employee.php
+++ src/FeatureEnvy/Trial/Employee.php
@@ -9,11 +9,6 @@ class Employee
     private float $pricePerHour;
     private string $kindOfBranchOffice;
 
-    /**
-     * @param string $name
-     * @param int $workedHours
-     * @param float $pricePerHour
-     */
     public function __construct(string $name, int $workedHours, float $pricePerHour)
     {
         $this->name = $name;
@@ -26,33 +21,21 @@ class Employee
         return $this->name;
     }
 
-    /**
-     * @return int
-     */
     public function getWorkedHours(): int
     {
         return $this->workedHours;
     }
 
-    /**
-     * @return float
-     */
     public function getPricePerHour(): float
     {
         return $this->pricePerHour;
     }
 
-    /**
-     * @return string
-     */
     public function getKindOfBranchOffice(): string
     {
         return $this->kindOfBranchOffice;
     }
 
-    /**
-     * @param string $kindOfBranchOffice
-     */
     public function setKindOfBranchOffice(string $kindOfBranchOffice): void
     {
         $this->kindOfBranchOffice = $kindOfBranchOffice;
